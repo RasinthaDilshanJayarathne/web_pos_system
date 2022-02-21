42a702680b824abf1c23d579750873ed3ab90cfd
Radintha Dilshan <rasinthadilshan9@gmail.com>
2022-02-21 15:49:56 +0530

    'Create PlaceOrderController.js'

--- controller/PlaceOrderController.js
+++ controller/PlaceOrderController.js
@@ -1,7 +1,9 @@
 $("#custChombo").click(function (){
+    $("#custChombo").empty();
     console.log("Enter");
     for (var i of customerDB){
-        let option =`<option>${i.id}</option>`;
-        $("#custChomboBody").append(option);
+        let option =`<option value="${i.id}">${i.id}</option>`;
+        $("#custChombo").append(option);
     }
+
 });
