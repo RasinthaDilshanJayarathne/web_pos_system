d1e3880138b2013a2ef6d8e03eb062a9970e1eda
Radintha Dilshan <rasinthadilshan9@gmail.com>
2022-02-21 11:01:19 +0530

    'Converted to layered Architecture'

---
+++ controller/DashboardController.js
@@ -0,0 +1,21 @@
+document.getElementById("customer").style.display = "none";
+document.getElementById("item").style.display = "none";
+document.getElementById("placeOrder").style.display = "block";
+
+document.getElementById("navHome").addEventListener("click", function () {
+    document.getElementById("placeOrder").style.display = "block";
+    document.getElementById("customer").style.display = "none";
+    document.getElementById("item").style.display = "none";
+});
+
+document.getElementById("navItem").addEventListener("click", function () {
+    document.getElementById("placeOrder").style.display = "none";
+    document.getElementById("customer").style.display = "none";
+    document.getElementById("item").style.display = "block";
+});
+
+document.getElementById("navCustomer").addEventListener("click", function () {
+    document.getElementById("placeOrder").style.display = "none";
+    document.getElementById("customer").style.display = "block";
+    document.getElementById("item").style.display = "none";
+});
