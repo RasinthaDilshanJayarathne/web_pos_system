63ce136bdb97cd5953383a9384111940f56a52f9
Radintha Dilshan <rasinthadilshan9@gmail.com>
2022-02-27 08:54:59 +0530

    'Update CustomerDTO.js'

--- model/CustomerDTO.js
+++ model/CustomerDTO.js
@@ -1,32 +1,35 @@
-function CustomerDTO(){
-    var customerId;
-    var customerName;
-    var customerAddress;
-    var customerPhone;
+function CustomerDTO(customerId, customerName, customerAddress, customerPhone) {
+    this.__cid = customerId;
+    this.__name = customerName;
+    this.__address = customerAddress;
+    this.__contact = customerPhone;
 
-    this.getCustomerId = function (){
-        return this.customerId;
+    this.getCustomerId = function () {
+        return this.__cid;
     }
-    this.setCustomerId = function (customerId){
-        this.customerId = customerId;
+    this.setCustomerId = function (customerId) {
+        this.__cid = customerId;
     }
-    this.getCustomerName = function (){
-        return this.customerName;
+
+    this.getCustomerName = function () {
+        return this.__name;
     }
-    this.setCustomerName = function (customerName){
-        this.customerName = customerName;
+    this.setCustomerName = function (customerName) {
+        this.__name = customerName;
     }
-    this.getCustomerAddress = function (){
-        return this.customerAddress;
+
+    this.getCustomerAddress = function () {
+        return this.__address;
     }
-    this.setCustomerAddress = function (customerAddress){
-        this.customerAddress = customerAddress;
+    this.setCustomerAddress = function (customerAddress) {
+        this.__address = customerAddress;
     }
-    this.getCustomerPhoneNo = function (){
-        return this.customerPhone;
+
+    this.getCustomerContact = function () {
+        return this.__contact;
     }
-    this.setCustomerPhoneNo = function (customerPhone){
-        this.customerPhone = customerPhone;
+    this.setCustomerContact = function (customerPhone) {
+        this.__contact = customerPhone;
     }
 }
 
